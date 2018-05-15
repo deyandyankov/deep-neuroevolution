8f6f21a66d33ddf0cd6965c4017ecd89b7caf545
Deyan Dyankov <deyan@dyankov.name>
2018-05-15 22:35:16 +0100

    Increase number of workers from 40 to 60

--- scripts/local_run_exp_workers.sh
+++ scripts/local_run_exp_workers.sh
@@ -4,5 +4,5 @@ ALGO=$1
 EXP_FILE=$2
 tmux new -s $NAME -d
 tmux send-keys -t $NAME '. scripts/local_env_setup.sh' C-m
-tmux send-keys -t $NAME 'python -m es_distributed.main workers --master_host 192.168.0.23 --relay_socket_path /tmp/es_redis_relay.sock --algo '$ALGO' --num_workers 40' C-m
+tmux send-keys -t $NAME 'python -m es_distributed.main workers --master_host 192.168.0.23 --relay_socket_path /tmp/es_redis_relay.sock --algo '$ALGO' --num_workers 60' C-m
 tmux a -t $NAME
