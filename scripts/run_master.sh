1c21c4cb63c02e7ec43bb7fc10735eebbfd98e9f
Deyan Dyankov <deyan.dyankov@vodafone.com>
2018-05-19 17:24:37 +0100

    fixes to convenience scripts

--- scripts/run_master.sh
+++ scripts/run_master.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 NAME=exp_`date "+%m_%d_%H_%M_%S"`
+ALGO=$1
 EXP_FILE=$2
 tmux new -s redis -d
 tmux send-keys -t redis 'redis-server redis_config/redis_master.conf' C-m
@@ -7,4 +8,4 @@ tmux split-window -t redis
 tmux send-keys -t redis 'redis-server redis_config/redis_local_mirror.conf' C-m
 tmux new -s $NAME -d
 tmux send-keys -t $NAME '. scripts/local_env_setup.sh' C-m
-tmux send-keys -t $NAME 'python -m es_distributed.main master --master_socket_path /tmp/es_redis_master.sock --log_dir 'log' --algo '$ALGO' --exp_file '"$EXP_FILE" C-m
+tmux send-keys -t $NAME 'python -m es_distributed.main master --master_socket_path /tmp/es_redis_master.sock --log_dir 'log' --algo '$ALGO' --exp_file $EXP_FILE ' C-m
