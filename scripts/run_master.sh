5ca333f952e4fb3ca811013a8d59c825a34fd558
Deyan Dyankov <deyan.dyankov@vodafone.com>
2018-05-19 17:28:33 +0100

    fixes to run_master

--- scripts/run_master.sh
+++ scripts/run_master.sh
@@ -8,4 +8,5 @@ tmux split-window -t redis
 tmux send-keys -t redis 'redis-server redis_config/redis_local_mirror.conf' C-m
 tmux new -s $NAME -d
 tmux send-keys -t $NAME '. scripts/local_env_setup.sh' C-m
-tmux send-keys -t $NAME 'python -m es_distributed.main master --master_socket_path /tmp/es_redis_master.sock --log_dir 'log' --algo '$ALGO' --exp_file $EXP_FILE ' C-m
+tmux send-keys -t $NAME "python -m es_distributed.main master --master_socket_path /tmp/es_redis_master.sock --log_dir log --algo ${ALGO} --exp_file ${EXP_FILE}" C-m
+
