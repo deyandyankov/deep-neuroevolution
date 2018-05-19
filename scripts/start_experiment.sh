a88094fbcc5ef433c9de22ba0094f87f276436c5
Deyan Dyankov <deyan.dyankov@vodafone.com>
2018-05-19 18:44:32 +0100

    change num_workers to 1

--- scripts/start_experiment.sh
+++ scripts/start_experiment.sh
@@ -4,10 +4,13 @@ worker_instances="inst1 inst2 inst3 inst4 inst5 inst6 inst7 inst8 inst9 inst10 i
 algo="nsr-es"
 configfile="configurations/frostbite_nsres.json"
 
+echo "Killing all user processes on master..."
 ssh $master_instance "pkill -u `whoami`"
+echo "Starting scripts/run_master.sh on master..."
 ssh -t $master_instance "cd deep-neuroevolution && git pull && . scripts/run_master.sh $algo $configfile"
 
 for worker in $worker_instances; do
+	echo "Initiating ${worker}"
 	ssh -t $worker "pkill -u `whoami`"
 	ssh -t $worker "cd deep-neuroevolution && git pull && . scripts/run_worker.sh $algo"
 done
