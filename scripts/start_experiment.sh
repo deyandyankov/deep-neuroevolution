595e935811bdaf2c23f84c578f7d04df488e3124
Deyan Dyankov <deyan.dyankov@vodafone.com>
2018-05-21 16:34:55 +0100

    apparently we should not start the workers too quickly...

--- scripts/start_experiment.sh
+++ scripts/start_experiment.sh
@@ -7,8 +7,10 @@ configfile="configurations/frostbite_nsres.json"
 echo "Killing all user processes on master..."
 echo "Starting scripts/run_master.sh on master..."
 ssh -t $master_instance "cd deep-neuroevolution && git pull && . scripts/run_master.sh $algo $configfile"
+sleep 60
 
 for worker in $worker_instances; do
 	echo "Initiating ${worker}"
 	ssh -t $worker "cd deep-neuroevolution && git pull && . scripts/run_worker.sh $algo" &
+	sleep 180
 done
